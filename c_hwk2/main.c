56430aa8ae470cb1499bec77685daa0f6b26a17f
Devchonka <menyaylenko@gmail.com>
2015-04-27 17:58:54 -0700

    first commit hwk 2 Advanced C

---
+++ c_hwk2/main.c
@@ -0,0 +1,38 @@
+/*********************************************************************************
+** CIS 26B                                                            Spring, 2015
+** Advanced C
+******************
+**
+** Homework 2: STOCKS
+**        A Circularly Doubly Linked List of Stacks
+**
+**********************************************************************************
+**  Written By: Menyaylenko, Elena
+**
+**  Date:               04/15/2015
+**  IDE (compiler):     GNU GCC Compiler, Unix OS
+**
+    Procedure:
+    1) xxx
+    x) Check memory leak detection using VALGRIND
+
+
+    To compile code:
+        gcc -std=c99  main.c defines.h defines.c -o c_hwk2
+    To check memory leaks with Valgrind:
+        valgrind --tool=memcheck --leak-check=yes ./c_hwk2
+************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main()
+{
+    char* fname_input = "hwk2_input.txt";
+
+
+
+    printf("Hello world!\n");
+    return 0;
+}
